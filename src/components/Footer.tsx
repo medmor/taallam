b885be55d789a41e25726b8e70d9b423a8a963af
medmor <enakreb@hotmail.com>
2023-06-12 21:49:43 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from "next-intl";
 export default function Footer() {
     const t = useTranslations('footer')
     return (
-        <div className="bg-white p-4 min-h-[15vh] flex justify-center items-center">
+        <div className="bg-white p-4 min-h-[15vh] flex flex-col justify-center items-center">
             <Logo src={t('logo')} /> &copy; 2023
             <div>All images from
                 <a href="https://www.freepik.com/">
