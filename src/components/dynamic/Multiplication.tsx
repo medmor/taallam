8858337a3af3cafcb055584c73ca7c8ff09df541
medmor <enakreb@hotmail.com>
2023-06-20 14:43:35 +0100

    multiplication

--- src/components/dynamic/Multiplication.tsx
+++ src/components/dynamic/Multiplication.tsx
@@ -1,11 +1,29 @@
-export default function Multiplication() {
-    const firstNumber = 5;
-    const secondNumber = 5;
+'use client'
+
+import { useCallback, useState } from "react";
+
+interface MultiplicationProps {
+    min: number;
+    max: number;
+}
+
+export default function Multiplication({ min, max }: MultiplicationProps) {
+    const [firstNumber, setFirstNumber] = useState(randomNumber(min, max));
+    const [secondNumber, setSecondNumber] = useState(randomNumber(min, max));
     const result: number | undefined = undefined;
+    const resetNumber = useCallback(() => {
+        setFirstNumber(randomNumber(min, max));
+        setSecondNumber(randomNumber(min, max))
+    }, [max, min])
     return (
         <div className={`p-10 rounded-lg flex flex-col items-center justify-center bg-white`}>
             <div>{firstNumber} x {secondNumber}</div>
             <div>{firstNumber * secondNumber}</div>
+            <button onClick={resetNumber}>refresh</button>
         </div>
     )
+}
+
+function randomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
