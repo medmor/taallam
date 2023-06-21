03539e3115f7539a4e9556e528921e22aa5f01d1
medmor <elmadioune@enekreb.com>
2023-06-21 15:33:23 +0100

    multiplication

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
     return (
         <div className="bg-white p-4 min-h-[15vh] flex flex-col justify-center items-center">
             <Logo src={t('logo')} /> &copy; 2023
-            <div>All images from
-                <a href="https://www.freepik.com/">
+            <div>Images from
+                <a className="mx-4 text-orange-600 font-semibold" target="blank" href="https://www.freepik.com/">
                     Freepik
                 </a>
             </div>
--- src/components/dynamic/Multiplication.tsx
+++ src/components/dynamic/Multiplication.tsx
@@ -1,29 +1,69 @@
 'use client'
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface MultiplicationProps {
-    min: number;
-    max: number;
+    properties: string[]
 }
 
-export default function Multiplication({ min, max }: MultiplicationProps) {
-    const [firstNumber, setFirstNumber] = useState(randomNumber(min, max));
-    const [secondNumber, setSecondNumber] = useState(randomNumber(min, max));
-    const result: number | undefined = undefined;
+export default function Multiplication({ properties }: MultiplicationProps) {
+    const numbers = useMemo(() => properties.map(n => Number(n)), [properties]);
+    const allNumbers = useMemo(() => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [])
+    const [firstNumber, setFirstNumber] = useState(0);
+    const [secondNumber, setSecondNumber] = useState(0);
+    const [answer, setAnswer] = useState(0);
+    const [wrong1, setWrong1] = useState(0)
+    const [wrong2, setWrong2] = useState(0)
+    const [score, setScore] = useState(0);
+    const [errors, setErrors] = useState(0);
     const resetNumber = useCallback(() => {
-        setFirstNumber(randomNumber(min, max));
-        setSecondNumber(randomNumber(min, max))
-    }, [max, min])
+        const numb1 = randomInNumbers(numbers);
+        const numb2 = randomInNumbers(allNumbers);
+        const res = numb1 * numb2;
+        const w1 = randomBetween(0, numbers[numbers.length - 1] * 10, res, res);
+        const w2 = randomBetween(0, numbers[numbers.length - 1] * 10, res, w1)
+        setFirstNumber(numb1);
+        setSecondNumber(numb2);
+        setAnswer(res);
+        setWrong1(w1)
+        setWrong2(w2)
+    }, [numbers, allNumbers]);
+    const checkAnswer = useCallback((ans: number) => {
+        if (ans == answer) {
+            setScore((score) => score + 1)
+        } else {
+            setErrors((errors) => errors + 1)
+        }
+        resetNumber();
+    }, [answer, resetNumber])
+    useEffect(() => {
+        resetNumber();
+    }, [resetNumber])
     return (
-        <div className={`p-10 rounded-lg flex flex-col items-center justify-center bg-white`}>
+        <div className={`p-10 rounded-lg flex flex-col items-center justify-center bg-white min-w-[300px]`} dir="ltr">
             <div>{firstNumber} x {secondNumber}</div>
-            <div>{firstNumber * secondNumber}</div>
+            <div>=</div>
+            <div className="flex gap-3">
+                <div onClick={() => checkAnswer(wrong1)}>{wrong1}</div>
+                <div onClick={() => checkAnswer(answer)} >{answer}</div>
+                <div onClick={() => checkAnswer(wrong2)}>{wrong2}</div>
+            </div>
+            <div>
+                Score : {score} Errors : {errors}
+            </div>
             <button onClick={resetNumber}>refresh</button>
         </div>
     )
 }
 
-function randomNumber(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min) + min);
+function randomInNumbers(numbers: number[]): number {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function randomBetween(min: number, max: number, dif1: number, dif2: number) {
+    const numb = Math.floor(Math.random() * (max - min) + min);
+    if (numb != dif1 && numb != dif2) {
+        return numb
+    } else {
+        return randomBetween(min, max, dif1, dif2)
+    }
 }
