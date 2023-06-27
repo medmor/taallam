1e9bce025b7383ffd91d01b4a4cddff9c00970a9
medmor <elmadioune@enekreb.com>
2023-06-27 14:07:25 +0100

    comment box

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import HtmlCommentsBox from "./HtmlCommentBox";
 import Logo from "./navbar/Logo";
 import { useTranslations } from "next-intl";
 
@@ -5,6 +6,8 @@ export default function Footer() {
     const t = useTranslations('footer')
     return (
         <div className="bg-white p-4 min-h-[15vh] flex flex-col justify-center items-center">
+            <HtmlCommentsBox />
+            <hr className="border-orange-200 my-8 w-full" />
             <Logo src={t('logo')} /> &copy; 2023
             <div>Images from
                 <a className="mx-4 text-orange-600 font-semibold" target="blank" href="https://www.freepik.com/">
---
+++ src/components/HtmlCommentBox.tsx
@@ -0,0 +1,58 @@
+import { useTranslations } from "next-intl"
+import Script from "next/script"
+export default function HtmlCommentsBox() {
+    const t = useTranslations("commentBox");
+    return (
+        <div className="shadow-lg bg-white p-4 mt-5 sm:min-w-[500px]">
+            <div id="HCB_comment_box" className="mt-2 ">
+                <div className="p-4 border border-orange-200">
+                    {t("refresh")}
+                </div>
+            </div>
+            <link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+            <Script id="htmlcommentsbox" strategy="lazyOnload">
+                {`
+                    if (!window.hcb_user) {
+                        if(window.location.href.includes('/ar')){
+                            hcb_user = {
+                                comments_header : "لا تتردد في ترك التعليق",
+                                name_label : 'الإ سم',
+                                content_label: 'أدخل تعليقك هنا',
+                                submit : 'تأكيد',
+                                no_comments_msg: "لم يعلق أحد حتى الآن. كن الاول!",
+                                add:'أضف تعليقك',
+                                again: 'اضف تعليقًا آخر',
+                                add_image:'أضف صورة',
+                                are_you_sure:'هل تريد وضع علامة على هذا التعليق على أنه غير ملائم؟',
+            
+                                reply:'أضف ردا',
+                                flag:'أضف تبليغ',
+                                like:"يعجبني",
+            
+                                days_ago:'أيام',
+                                hours_ago:'ساعات',
+                                minutes_ago:'دقائق',
+                                within_the_last_minute:'في الدقيقة الأخيرة',
+            
+                                msg_thankyou:"شكرا على التعليق",
+                                msg_approval:"ملاحظة: لا يتم نشر هذا التعليق حتى تتم الموافقة عليه",
+                                msg_approval_required:"شكرا على التعليق! سيظهر تعليقك بمجرد الموافقة عليه من قبل الوسيط."
+                            }; 
+                        }
+                        else{
+                            { hcb_user = {}; }
+                        }
+                    }
+                    (function(){
+                        var s=document.createElement("script"),
+                        l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"),
+                        h="https://www.htmlcommentbox.com";
+                        s.setAttribute("type","text/javascript");
+                        s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24ULEEOXDwJOmf1X6137mqN."+"&opts=16798&num=10&ts=1687870882651");
+                        if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})();
+            `}
+            </Script>
+
+        </div>
+    )
+}
