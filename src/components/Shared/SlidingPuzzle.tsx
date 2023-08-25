a5570af3180697da47df35af474fac7815df587c
medmor <elmadioune@enekreb.com>
2023-08-25 15:20:22 +0100

    sliding puzzle

--- src/components/Shared/SlidingPuzzle.tsx
+++ src/components/Shared/SlidingPuzzle.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import MiniGame from "./MiniGame"
 import { oneDTo2D, twoDTo1D } from "@/helpers/array"
 import { Directions } from "@/types/Directions"
+import { animated, useSpring } from '@react-spring/web'
 
 interface SlidingPuzzleProps {
     items: string[]
@@ -22,7 +23,52 @@ const SlidingPuzzle = (props: SlidingPuzzleProps) => {
             )
     )
 
+
+
+    return (
+        // <MiniGame saveKey={"SlidingPuzzle"+props.items.toString()}>
+        <div className={`grid grid-cols-${gridSize} w-[300px] h-[300px] m-auto gap-3`}>
+            {cells.map(cell => (
+                cell.item != freeCell && (
+                    <CellComponent key={cell.item} cell={cell} cells={cells} setCells={setCells} freeCell={freeCell} gridSize={gridSize} />
+                )
+            ))}
+        </div>
+        // </MiniGame>
+    )
+}
+export default SlidingPuzzle
+
+////////////////////////////////////////Cell Component
+
+interface CellComponentProps {
+    cell: Cell
+    cells: Cell[]
+    setCells: any
+    freeCell: string
+    gridSize: number
+}
+const CellComponent = (props: CellComponentProps) => {
+
+    const [hover, setHover] = useState(false)
+    const [direction, setDirection] = useState<Directions>()
+    const { x } = useSpring({
+        x: hover ? 1 : 0,
+        config: { duration: 500 },
+    })
+    const [springs, api] = useSpring(()=>({
+        from: {x:0}
+    }))
+
     const handleCellClick = (direction?: Directions) => {
+        api.start({
+            from: {
+              x: 0,
+            },
+            to: {
+              x: 100,
+            },
+          })
         if (!direction) {
             return
         }
@@ -43,14 +89,43 @@ const SlidingPuzzle = (props: SlidingPuzzleProps) => {
         }
     }
 
+    const slideDir = (): Directions | undefined => {
+        if (
+            props.cell.x < props.gridSize - 1
+            && props.cells[(twoDTo1D(props.cell.x + 1, props.cell.y, props.gridSize))].item == props.freeCell
+        ) {
+            return 'right';
+        }
+        else if (
+            props.cell.x > 0
+            && props.cells[(twoDTo1D(props.cell.x - 1, props.cell.y, props.gridSize))].item == props.freeCell
+        ) {
+            return 'left'
+        }
+        else if (
+            props.cell.y < props.gridSize - 1
+            && props.cells[(twoDTo1D(props.cell.x, props.cell.y + 1, props.gridSize))].item == props.freeCell
+        ) {
+            return 'bottom'
+        } else if (
+            props.cell.y > 0
+            && props.cells[(twoDTo1D(props.cell.x, props.cell.y - 1, props.gridSize))].item == props.freeCell
+        ) {
+            return 'top'
+        }
+    }
+
     return (
-        // <MiniGame saveKey={"SlidingPuzzle"+props.items.toString()}>
-        <div className={`grid grid-cols-${gridSize} w-[300px] h-[300px] m-auto gap-3`}>
-            {cells.map(cell => (
-                cell.item != freeCell && (
-                    <div
-                        key={cell.item}
-                        className="
+        <animated.div
+            onMouseEnter={()=>setHover(hover=>!hover)}
+            style={{
+                scale: x.to({
+                    range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                    output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+                }),
+                ...springs
+            }}
+            className="
                     bg-indigo-500
                     rounded-xl
                     flex
@@ -59,19 +134,13 @@ const SlidingPuzzle = (props: SlidingPuzzleProps) => {
                     text-4xl
                     font-bold
                     cursor-pointer
-                    active:animate-bounce
                     "
-                        onClick={() => handleCellClick(cell.slideDir(cells, freeCell, gridSize))}
-                    >
-                        {cell.item}
-                    </div>
-                )
-            ))}
-        </div>
-        // </MiniGame>
+            onClick={() => handleCellClick(slideDir())}
+        >
+            {props.cell.item}
+        </animated.div>
     )
 }
-export default SlidingPuzzle
 
 //////////////////////////////////////////Model
 class Cell {
@@ -81,17 +150,6 @@ class Cell {
         public y: number
     ) {
     }
-
-    slideDir(cells: Cell[], freeCell: string, size: number): Directions | undefined {
-        if (this.x < size - 1 && cells[(twoDTo1D(this.x + 1, this.y, size))].item == freeCell) {
-            return 'right';
-        } else if (this.x > 0 && cells[(twoDTo1D(this.x - 1, this.y, size))].item == freeCell) {
-            return 'left'
-        } else if (this.y < size - 1 && cells[(twoDTo1D(this.x, this.y + 1, size))].item == freeCell) {
-            return 'bottom'
-        } else if (this.y > 0 && cells[(twoDTo1D(this.x, this.y - 1, size))].item == freeCell) {
-            return 'top'
-        }
-    }
 }
 
+////////////////////////////////////////////Helpers
