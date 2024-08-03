ac461eef525851e8d3deb9b9a55528c13e6a32a9
medmor <enakreb@hotmail.com>
2024-08-03 14:00:10 +0100

    games

---
+++ src/app/[locale]/games/page.tsx
@@ -0,0 +1,14 @@
+export default async function GamesPage() {
+
+    return (
+        <>
+            <div className="w-full h-screen flex justify-center items-center">
+                <iframe
+                    src="https://itch.io/embed-upload/4024870?color=333333"
+                    allowFullScreen width="960" height="620">
+                    <a href="https://medmor.itch.io/maze-learn">Play Maze Learn on itch.io</a>
+                </iframe>
+            </div>
+        </>
+    );
+}
