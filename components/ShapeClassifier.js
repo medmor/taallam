82ff510eb79fb96507b7cdbe7255cec55ac25b65
medmor <elmadioune@gmail.com>
2025-09-02 19:23:59 +0100

    feat: update SVG properties and shape generation logic in ShapeClassifier

--- components/ShapeClassifier.js
+++ components/ShapeClassifier.js
@@ -257,12 +257,18 @@ export default function ShapeClassifier() {
       angles: 0,
       type: "curve",
       svg: (
-        <svg width="80" height="80" viewBox="0 0 80 80">
+        <svg
+          width="80"
+          height="80"
+          viewBox="0 -1 17 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
-            d="M40,10 A30,30 0 1,1 40,70 A20,30 0 1,0 40,10"
+            d="M7.7066 0.00274765C7.50391 -0.0027381 7.31797 0.114737 7.23588 0.300147C7.15379 0.485558 7.19181 0.702186 7.33213 0.848565C8.36577 1.92686 9.00015 3.3888 9.00015 4.99996C9.00015 8.31366 6.31385 11 3.00015 11C2.5757 11 2.16207 10.956 1.76339 10.8725C1.56489 10.8309 1.36094 10.9133 1.2471 11.0812C1.13325 11.249 1.13207 11.469 1.2441 11.638C2.58602 13.663 4.88682 15 7.50012 15C11.6423 15 15.0001 11.6421 15.0001 7.49996C15.0001 3.42688 11.7534 0.112271 7.7066 0.00274765Z"
             fill="#fff176"
             stroke="#fbc02d"
-            strokeWidth="2"
+            strokeWidth="0.5"
           />
         </svg>
       ),
@@ -369,7 +375,7 @@ export default function ShapeClassifier() {
 
   // Generate shapes for current level
   const generateLevelShapes = () => {
-    const shuffledShapes = [...shapes].filter((s) => s.id === "heart"); //[...shapes].sort(() => Math.random() - 0.5);
+    const shuffledShapes = [...shapes].filter((s) => s.id === "crescent"); //[...shapes].sort(() => Math.random() - 0.5);
     return shuffledShapes.slice(0, Math.min(shapesPerLevel, shapes.length));
   };
 
@@ -592,7 +598,7 @@ export default function ShapeClassifier() {
                   >
                     <Box
                       sx={{
-                        p: 1,
+                        p: 2,
                         backgroundColor: "background.paper",
                         borderRadius: 2,
                         boxShadow: 1,
